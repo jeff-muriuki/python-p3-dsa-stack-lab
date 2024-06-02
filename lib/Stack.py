3ff194fd4f9c2c69e2d9a6a4d47d2d352be7433a
jeff muriuki <jeff.muriuki@studet.moringaschool.com>
2024-06-02 21:23:01 +0300

    initial

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,35 @@
 class Stack:
-
-    def __init__(self, items = [], limit = 100):
-        pass
+    def __init__(self, items= [], limit=100):
+        self.items = items
+        self.limit = limit
 
     def isEmpty(self):
-        pass
+        return len(self.items)== 0
 
     def push(self, item):
-        pass
-
+        if len(self.items) < self.limit:
+            self.items.append(item)
+        else:
+            return None
+    
     def pop(self):
-        pass
-
+        if not self.isEmpty():
+            return self.items.pop()
+        else:
+            return None
+             
     def peek(self):
         pass
     
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        return len(self.items) == self.limit
+
 
     def search(self, target):
-        pass
+        try:
+            return len(self.items) - self.items.index(target) -1
+        except ValueError:
+            return -1
